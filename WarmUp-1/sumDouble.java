61ff83dd6c5e73f80320a5b44c4318dbda3ef9b8
Mari Hirota <mari.hirota97@gmail.com>
2023-07-05 23:05:05 -0500

    Push some more problem solution

---
+++ WarmUp-1/sumDouble.java
@@ -0,0 +1,12 @@
+/*
+ * Given two int values, return their sum. Unless the two 
+ * values are the same, then return double their sum.
+ */
+public int sumDouble(int a, int b) {
+  if (a == b){
+    return 2*(a+b);
+  }
+  else{
+    return a+b;
+  }
+}
