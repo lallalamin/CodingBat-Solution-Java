a21f927dd9f22cc4427dbc51577d4f7a983393b0
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2023-07-08 23:55:24 -0500

    Add files via upload

---
+++ posNeg.java
@@ -0,0 +1,23 @@
+/*
+ * Given 2 int values, return true if one is negative and one
+ * is positive. Except if the parameter "negative" is true, 
+ * then return true only if both are negative.
+ */
+public boolean posNeg(int a, int b, boolean negative) {
+  if (negative == true){
+    if (a < 0 && b < 0){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  else{
+    if (a < 0 && b > 0 || a > 0 && b < 0){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
