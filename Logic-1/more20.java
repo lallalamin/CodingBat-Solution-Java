de7323c4356be4c24621fb78f2e745352760ec47
Mari Hirota <mari.hirota97@gmail.com>
2023-07-28 00:02:42 -0500

    Add 5 solutions for Logic-1

---
+++ Logic-1/more20.java
@@ -0,0 +1,8 @@
+/*
+ * Return true if the given non-negative number is 1 or 2 more than 
+ * a multiple of 20. See also: Introduction to Mod
+ */
+
+ public boolean more20(int n) {
+  return (n%20 == 1 || n%20 == 2);
+}
