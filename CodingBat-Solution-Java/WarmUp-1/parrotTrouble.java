e4869b9165cfa7765f250ca7122de95df7e117b1
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2023-08-27 23:34:26 -0500

    Delete CodingBat-Solution-Java/WarmUp-1 directory

--- CodingBat-Solution-Java/WarmUp-1/parrotTrouble.java
+++
@@ -1,9 +0,0 @@
-/*
- * We have a loud talking parrot. The "hour" parameter is the
- * current hour time in the range 0..23. We are in trouble if 
- * the parrot is talking and the hour is before 7 or after 20. 
- * Return true if we are in trouble.
- */
-public boolean parrotTrouble(boolean talking, int hour) {
-  return (talking && (hour > 20 || hour <7));
-}
