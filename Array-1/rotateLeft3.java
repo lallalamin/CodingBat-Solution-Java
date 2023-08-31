017f265f99abbafcb1267abc484d1d74796412ad
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2023-08-30 23:37:12 -0500

    add 3 solutions to array 1

---
+++ Array-1/rotateLeft3.java
@@ -0,0 +1,9 @@
+/*
+ * Given an array of ints length 3, return an array with the elements "rotated left" so 
+ * {1, 2, 3} yields {2, 3, 1}.
+ */
+
+public int[] rotateLeft3(int[] nums) {
+  int[] rotate = {nums[1], nums[2], nums[0]};
+  return rotate;
+}
