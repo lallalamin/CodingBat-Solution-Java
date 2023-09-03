94a4042f1e10892c2accc0b84523c380e5b421ca
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2023-09-02 23:40:03 -0500

    Add 3 solutions to Array 1

---
+++ Array-1/reverse3.java
@@ -0,0 +1,9 @@
+/*
+ * Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} 
+ * becomes {3, 2, 1}.
+ */
+
+public int[] reverse3(int[] nums) {
+  int[] reverse = {nums[2], nums[1], nums[0]};
+  return reverse;
+}
